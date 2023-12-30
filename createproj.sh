cfb37813a90f3fafa1c9530a5863dad8b0fdae50
Guillaume MICHEL <guillaume@MacBook-Pro-de-Guillaume.local>
2023-12-30 11:00:21 +0100

    [+] update createproj.sh

--- createproj.sh
+++ createproj.sh
@@ -10,26 +10,20 @@ fi
 project_name=$1
 project_path=$2
 
-# Check if the directory already exists
-if [ -d "$project_path/$project_name" ]; then
-    echo "The directory $project_name already exists at path $project_path."
-    exit 1
-fi
-
-# Create the project directory
+# Create the project directory (or skip if it already exists)
 mkdir -p "$project_path/$project_name"
 
 # Change into the project directory
 cd "$project_path/$project_name" || exit
 
-# URLs des fichiers .gitignore sur GitHub
+# URLs of .gitignore files on GitHub
 gitignore_jetbrains_url="https://raw.githubusercontent.com/github/gitignore/main/Global/JetBrains.gitignore"
 gitignore_vscode_url="https://raw.githubusercontent.com/github/gitignore/main/Global/VisualStudioCode.gitignore"
 gitignore_c_url="https://raw.githubusercontent.com/github/gitignore/main/C.gitignore"
 gitignore_cpp_url="https://raw.githubusercontent.com/github/gitignore/main/C%2B%2B.gitignore"
 gitignore_haskell_url="https://raw.githubusercontent.com/github/gitignore/main/Haskell.gitignore"
 
-# Téléchargement des fichiers .gitignore
+# Downloading the .gitignore files
 curl -o JetBrains.gitignore "$gitignore_jetbrains_url"
 curl -o VisualStudioCode.gitignore "$gitignore_vscode_url"
 curl -o C.gitignore "$gitignore_c_url"
@@ -40,7 +34,10 @@ cat JetBrains.gitignore VisualStudioCode.gitignore C.gitignore C++.gitignore Has
 
 rm JetBrains.gitignore VisualStudioCode.gitignore C.gitignore C++.gitignore Haskell.gitignore
 
+# Remove duplicate lines from .gitignore and save the result to a temporary file
 awk '!seen[$0]++' .gitignore > temp_gitignore
+
+# Replace the original .gitignore with the cleaned version
 mv temp_gitignore .gitignore
 
 # Copy another script from an existing folder on your PC
